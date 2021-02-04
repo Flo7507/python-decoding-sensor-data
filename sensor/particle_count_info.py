812bb826a6b151d049f24970be4c5b92ffc32980
Valette <floris_valette@hotmail.com>
2021-02-04 22:51:44 +0100

    Part #5-2

--- sensor/particle_count_info.py
+++ sensor/particle_count_info.py
@@ -17,6 +17,13 @@ class ParticleData(HouseInfo):
         return self._convert_data(recs)
 
     def get_data_concentrations(self, data):
-            pass()
+        particulate = {"good" : 0, "moderate" : 0, "bad" : 0}
+        for rec in data:
+            if rec <= 50.0: 
+                particulate["good"] += 1
+            elif rec > 50.0 and rec <= 100.0:
+                particulate["moderate"] += 1
+            else:
+                particulate["bad"] += 1
+        return particulate
 
-        
